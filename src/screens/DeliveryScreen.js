1a7b47a3072c5dee940fe95804daefa4085c75f4
WebZerefose <anything.codable@gmail.com>
2023-05-12 11:28:19 -0300

    Feat:  create Components

--- src/screens/DeliveryScreen.js
+++ src/screens/DeliveryScreen.js
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { featured } from '../contants'
 
 const DeliveryScreen = () => {
+	const restaurant = featured.restaurants[0]
+
 	return (
 		<View>
 			<Text>DeliveryScreen</Text>
